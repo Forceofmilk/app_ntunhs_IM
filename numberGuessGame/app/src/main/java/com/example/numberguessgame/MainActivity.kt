96018433b84b652858feeefc68ec06d39263ca3e
Forceofmilk <thomasthedreamer1019@gmail.com>
2024-03-14 13:03:34 +0800

    0314_Version 2.0
    1. app 設為多國語言 (中文/英文)
    2. 猜到結果後，等待 6 秒進行遊戲重製，並且跳出提醒
    3. 按鈕改為圓角，並取在按下後會有變化

--- numberGuessGame/app/src/main/java/com/example/numberguessgame/MainActivity.kt
+++ numberGuessGame/app/src/main/java/com/example/numberguessgame/MainActivity.kt
@@ -2,6 +2,8 @@ package com.example.numberguessgame
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.widget.TextView
 import android.widget.Button
 import android.widget.EditText
@@ -10,9 +12,12 @@ import androidx.appcompat.app.AlertDialog
 import java.util.*
 
 class MainActivity : AppCompatActivity() {
+    val TAG:String = MainActivity::class.java.simpleName
+    private lateinit var handler: Handler
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        handler = Handler(Looper.getMainLooper())
 
         val textView_hint = findViewById<TextView>(R.id.hint)
         val editText_userInput = findViewById<EditText>(R.id.userInput)
@@ -24,6 +29,15 @@ class MainActivity : AppCompatActivity() {
         var guess_min : Int = 1
         var guessRound : Int = 0
 
+        fun resetGame() {
+            Toast.makeText(this, "遊戲已重置",Toast.LENGTH_LONG).show()
+            secretNum = Random().nextInt(100)+1
+            textView_hint.text = "猜 1 ~ 100 之間的數字"
+            guess_max = 100
+            guess_min = 1
+            guessRound = 0
+        }
+
         button_confirm.setOnClickListener {
             var guessNum = editText_userInput.text.toString().toInt()
             var hintWords : String = ""
@@ -33,6 +47,16 @@ class MainActivity : AppCompatActivity() {
                 hintWords = "厲害! 你猜對囉"
                 guessRound++
                 AlertDialog.Builder(this).setTitle("恭喜過關").setMessage("終極密碼是 "+secretNum+"\n你總共猜了 "+guessRound+" 次才猜中 !").create().show()
+                handler.postDelayed({
+                    resetGame()
+                    Toast.makeText(this,"5 秒後的操作執行了 ! ",Toast.LENGTH_LONG).show()},
+                    6000)
+
+
+                fun onDestroy() {
+                    super.onDestroy()
+                    handler.removeCallbacksAndMessages(null)
+                }
             }
             else if (guessNum < secretNum) {
                 guess_min = guessNum
@@ -44,17 +68,17 @@ class MainActivity : AppCompatActivity() {
                 hintWords = "太大囉，猜 "+guess_min+" ~ "+guess_max+" 之間的數字"
                 guessRound++
             }
+            else{
+                hintWords = "請輸入有效數字"
+                guessRound++
+            }
             textView_hint.text = hintWords
         }
 
         button_reset.setOnClickListener {
-            Toast.makeText(this, "遊戲已重置",Toast.LENGTH_LONG).show()
-            secretNum = Random().nextInt(100)+1
-            textView_hint.text = "猜 1 ~ 100 之間的數字"
-            guess_max = 100
-            guess_min = 1
-            guessRound = 0
-
+            resetGame()
         }
+
+
     }
 }
